25ff7ef73a9538a4c16c472eb7962e2addc192ef
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-06-26 20:07:49 +0700

    Setup Networking in the scene

--- Assets/_Data/Scripts/PlayerMovement_PA.cs
+++ Assets/_Data/Scripts/PlayerMovement_PA.cs
@@ -5,11 +5,22 @@ public class PlayerMovement_PA : MonoBehaviour
 {
     [SerializeField] protected CharacterController _characterController;
     [SerializeField] protected InputActionReference _moveInput;
+    [SerializeField] protected float _speed = 2f;
+
+    [SerializeField] protected InputActionReference _lookInput;
+    [SerializeField] protected float _rotationSpeed = 45f;
 
     void Update()
     {
+        _characterController.Move(GetMoveDirection() * _speed * Time.deltaTime);
+        var lookValues = _lookInput.action.ReadValue<Vector2>();
+        transform.Rotate(0, lookValues.x * _rotationSpeed * Time.deltaTime, 0);
+    }
+
+    protected Vector3 GetMoveDirection()
+    {
         var inputValues = _moveInput.action.ReadValue<Vector2>();
         var direction = transform.forward * inputValues.y + transform.right * inputValues.x;
-        direction = direction.normalized;
+        return direction.normalized;
     }
 }
