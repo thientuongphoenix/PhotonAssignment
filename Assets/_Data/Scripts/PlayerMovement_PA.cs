289e46dc7a7ae89068f49394ffef8c00f9d1cc49
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-06-28 15:31:08 +0700

    Adjust Player Movement

--- Assets/_Data/Scripts/PlayerMovement_PA.cs
+++ Assets/_Data/Scripts/PlayerMovement_PA.cs
@@ -1,26 +1,94 @@
+using Fusion;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement_PA : MonoBehaviour
+public class PlayerMovement_PA : NetworkBehaviour, ISpawned
 {
-    [SerializeField] protected CharacterController _characterController;
-    [SerializeField] protected InputActionReference _moveInput;
-    [SerializeField] protected float _speed = 2f;
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private NetworkCharacterController _networkController;
+    [SerializeField] private InputActionReference _moveInput;
+    [SerializeField] private InputActionReference _sprintInput;
+    [SerializeField] private InputActionReference _jumpInput;
+    //[FormerlySerializedAs("_speed")]
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _sprintSpeed;
+    [SerializeField] private float _rotateSpeed;
+    //[SerializeField] private Animator _anim;
 
-    [SerializeField] protected InputActionReference _lookInput;
-    [SerializeField] protected float _rotationSpeed = 45f;
+    [SerializeField] private InputActionReference _lookInput;
 
-    void Update()
+    private bool _isJump;
+    private float _rotateX;
+
+    [Networked, OnChangedRender(nameof(OnVelocityChanged))]
+    private Vector3 Velocity { get; set;}
+
+    private void OnVelocityChanged()
     {
-        _characterController.Move(GetMoveDirection() * _speed * Time.deltaTime);
-        var lookValues = _lookInput.action.ReadValue<Vector2>();
-        transform.Rotate(0, lookValues.x * _rotationSpeed * Time.deltaTime, 0);
+        //_anim.SetFloat("Speed", Velocity.magnitude);
     }
 
-    protected Vector3 GetMoveDirection()
+    public override void Spawned()
+    {
+        base.Spawned();
+        if (!HasStateAuthority) { return; }
+
+        _characterController.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (!HasStateAuthority) { return; }
+
+        Velocity = GetMoveDirection();
+
+        if (_sprintInput.action.IsPressed())
+        {
+            Velocity *= _sprintSpeed;
+        }
+        else
+        {
+            Velocity *= _walkSpeed;
+        }
+
+        if (_jumpInput.action.triggered)
+        {
+            _isJump = true;
+        }
+
+        var lookValues = _lookInput.action.ReadValue<Vector2>();
+        _rotateX += lookValues.x * _rotateSpeed * Time.deltaTime;
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        base.FixedUpdateNetwork();
+
+        if (!HasStateAuthority) { return; }
+        NetworkUpdateMovement();
+        NetworkUpdateRotation();
+    }
+
+    private void NetworkUpdateMovement()
+    {
+        _networkController.Move(Velocity);
+        if (_isJump)
+        {
+            _networkController.Jump();
+            _isJump = false;
+        }
+    }
+
+    private Vector3 GetMoveDirection()
     {
         var inputValues = _moveInput.action.ReadValue<Vector2>();
         var direction = transform.forward * inputValues.y + transform.right * inputValues.x;
         return direction.normalized;
+    }
+
+    private void NetworkUpdateRotation()
+    {
+        transform.Rotate(0, _rotateX, 0);
+        _rotateX = 0;
     }
 }
