422d3d9445f548d72d8002e93c391bdea0bed79f
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-07-06 19:08:36 +0700

    .

---
+++ Assets/_Data/Scripts/QuitBtn.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class QuitBtn : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
